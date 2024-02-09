01a405d5bbfd315384e1a9b6f684db9150a456e6
Ecanno8 <ethancannon92@gmail.com>
2024-02-09 09:58:41 -0800

    error handling

--- index.jsx
+++ index.jsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from 'components\main-view\main-view.jsx';
 
-// Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
