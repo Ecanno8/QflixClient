1b9a8f2512293d1f686e80dfc0d8104c336aa9e5
Ecanno8 <ethancannon92@gmail.com>
2024-02-13 15:24:24 -0800

    open array connect to api

---
+++ components/main-view/main-view.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("https://movieflix-bf9931a77954.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                const QflixApi = data.map((movie) => {
+                    return {
+                        _id: movie._id,
+                        Title: movie.Title,
+                        ImagePath: movie.ImagePath,
+                        Description: movie.Description,
+                        Year: movie.Year,
+                        Genre: {
+                            Name: movie.Genre.Name
+                        },
+                        Director: {
+                            Name: movie.Director.Name
+                        }
+                    };
+                });
+                setMovies(QflixApi);
+            });
+    }, []);
+
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    if (selectedMovie) {
+        return (
+            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        );
+    }
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((movie) => (
+                <MovieCard
+                    key={movie._id}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />
+            ))}
+        </div>
+    );
+};
