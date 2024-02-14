32d0a40e6ab2e772d12c3a132a72352d15d2c551
Ecanno8 <ethancannon92@gmail.com>
2024-02-14 13:57:28 -0800

    bonus task selected movies

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -6,6 +6,7 @@ import { LoginView } from "../login-view/login-view";
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
 
+
     useEffect(() => {
         fetch("https://movieflix-bf9931a77954.herokuapp.com/movies")
             .then((response) => response.json())
@@ -33,9 +34,34 @@ export const MainView = () => {
     const [selectedMovie, setSelectedMovie] = useState(null);
 
     if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
+        let similarMovies = movies.filter((movie) => {
+            return movie._id !== selectedMovie._id && movie.Genre.Name === selectedMovie.Genre.Name;
+        });
+        if (similarMovies.length === 0) {
+            return (
+                <>
+                    <MovieView movie={selectedMovie} onBackClick={() => setselectedMovie(null)} /><br />
+                    <h2>Similar Movies</h2>
+                    <p>There are no similar movies.</p>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <MovieView movie={selectedMovie} onBackClick={() => setselectedMovie(null)} /><br />
+                    <h2>Similar Movies</h2>
+                    {similarMovies.map((movie) => (
+                        <MovieCard
+                            key={movie._id}
+                            movie={movie}
+                            onMovieClick={(newSelectedMovie) => {
+                                setselectedMovie(newSelectedMovie);
+                            }}
+                        />
+                    ))}
+                </>
+            );
+        }
     }
 
     if (movies.length === 0) {
